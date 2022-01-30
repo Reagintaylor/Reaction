4be3904ada64935f32a162a1cc7d0d49ca3bc9f2
Reagin Turner <rturnerafteragnes@gmail.com>
2022-01-30 17:58:31 -0500

    editing content

---
+++ reaction/README.md
@@ -0,0 +1,17 @@
+# Reaction
+my react.js portfolio 
+
+
+npx create-react-app "name of app"
+npm run build?
+npm start 
+
+
+to uninstall run
+npm uninstall -g create-react-app
+
+todos
+redo the css
+add my content and redo the pages
+
+Find a nice font and style the pages
---
+++ reaction/src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Reagin</h1>
+      <p>
+        " Insert emoji" Software Developer with a passion for turning ideas into reults. 
+
+
+        
+      </p>
+    </div>
+  );
+}
