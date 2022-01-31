43ae27fb2ea3ae158d783e526380dd39dc4c39fa
Reagin Turner <rturnerafteragnes@gmail.com>
2022-01-31 16:08:06 -0500

    Adding footer links and icons

--- reaction/src/components/Footer.js
+++ reaction/src/components/Footer.js
@@ -3,9 +3,9 @@ import React from 'react';
 export default function Footer() {
   return (
     <div>
-      <img className="" alt="" src=""/>
-      <img className="" alt="" src=""/>
-      <img className="" alt="" src=""/>
+      <a href="https://github.com/Reagintaylor"><img className="github" alt="github-logo" src="../images/github.png"/></a>
+      <a href="https://www.linkedin.com/in/reagin-turner-01068b16b/"><img className="linkedin" alt="linkedin-logo" src="../images/linkedin (1).png"/></a>
+      <a href="#"><img className="twitter" alt="twitter-logo" src="../images/twitter.png"/></a>
     </div>
   );
 }
